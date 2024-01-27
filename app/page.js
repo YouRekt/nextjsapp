48741304edf80f89c33f84bbca027a2088b190ff
YouRekt <czarnecki.jurko@gmail.com>
2024-01-27 02:26:16 +0100

    inital commit

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-7xl">Home Page</h1>
+      <Link href="/about" className="text-2xl">
+        AboutPage
+      </Link>
+    </div>
+  );
+};
+
+export default HomePage;
